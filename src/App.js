bf924ef3dca8cff90b3105bbda6e75b75ee69feb
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2022-08-01 11:29:42 -0400

    navbar component from tailwind

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="">
+      <Navbar />
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='login' element={<Login />} />
